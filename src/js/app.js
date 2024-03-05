eb1aaf9c916b7c16ebf5a25202a874ec2f836668
Inti Arias <73969772+intiarias@users.noreply.github.com>
2024-03-05 17:32:43 +0000

    cambio final

--- src/js/app.js
+++ src/js/app.js
@@ -27,15 +27,18 @@ function render(variables = {}) {
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == true) cover = "<div class='cover'></div>";
+  if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name}  </h1>
+          <h1>${variables.name}</h1>
+          <h2>${variables.lastName}</h2>
           <h2>${variables.role}</h2>
-          <h3>${variables.country}</h3>
+          <h2>${variables.country}</h2>
+          <h2>${variables.city}</h2>
+
           <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
